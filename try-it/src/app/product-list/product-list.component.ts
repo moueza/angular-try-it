7b9e1633df60d68a319c3f4e9729040051051e54
moueza <>
2020-08-22 09:26:38 +0200

    See #1 general : local server OK. Resolves #2 P1 A sample App

--- try-it/src/app/product-list/product-list.component.ts
+++ try-it/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products = products;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  share() {
+    window.alert('The product has been shared!');
+  }
 }
